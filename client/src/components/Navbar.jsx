9f9435fa4bbc82dfde5aa65a705dfc66aba2f66b
jsh3418 <albert5428@gmail.com>
2022-12-21 16:38:31 +0900

    feat: Navbar 로고버튼 구현

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
 function Navbar(props) {
-  return <div></div>;
+  return (
+    <>
+      {/* 컨테이너 */}
+      <header className="box-border h-[50px] fixed left-0 top-0 min-w-[auto] shadow-md w-full z-50 bg-white	flex border-t-[3px] border-[#f48225] items-center">
+        <div className="w-[97.2307692rem] max-w-full h-full flex my-0 mx-auto items-center box-border">
+          {/* 로고 */}
+          <a
+            href="#"
+            className="py-0 px-2 h-full flex items-center bg-transparent box-border"
+          >
+            <span className="bg-no-repeat bg-left-bottom ml-0 w-[150px] h-[30px] mt-[-4px] inline-block bg-[url('https://cdn.sstatic.net/Img/unified/sprites.svg?v=fcc0ea44ba27')]"></span>
+          </a>
+          {/* About, Products, For Teams 버튼 */}
+          {/* 검색창 자리 */}
+          {/* 로그인 */}
+          {/* 회원가입 */}
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default Navbar;
