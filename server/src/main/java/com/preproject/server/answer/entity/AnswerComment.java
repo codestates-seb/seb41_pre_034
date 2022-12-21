3f2a4b26c88df059d7842b0d5579d7b02408ef6c
Choiyu330 <jenny4063@naver.com>
2022-12-21 17:24:17 +0900

    feat: AnswerComment - Entity 생성

---
+++ server/src/main/java/com/preproject/server/answer/entity/AnswerComment.java
@@ -0,0 +1,39 @@
+package com.preproject.server.answer.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "createAt"),
+        @Index(columnList = "updateAt")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class AnswerComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long answerCommentId;
+
+    @Column(nullable = false)
+    private String comment;
+
+    @Column(nullable = false, insertable = false, updatable = false,
+            columnDefinition = "datetime default CURRENT_TIMESTAMP")
+    @CreatedDate
+    private LocalDateTime createAt;
+
+    @Column(nullable = false, insertable = false, updatable = false,
+            columnDefinition = "datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP")
+    @LastModifiedDate
+    private LocalDateTime updateAt;
+}
