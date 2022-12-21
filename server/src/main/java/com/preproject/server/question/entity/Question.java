7f935ff83d58564458844c3969b0bba9e9bbd278
sussa3007 <sussa1933@gmail.com>
2022-12-21 20:48:43 +0900

    feat: Question & Tag 연관 관계 매핑

--- server/src/main/java/com/preproject/server/question/entity/Question.java
+++ server/src/main/java/com/preproject/server/question/entity/Question.java
@@ -1,8 +1,11 @@
 package com.preproject.server.question.entity;
 
 
+import com.preproject.server.answer.entity.Answer;
 import com.preproject.server.constant.QuestionStatus;
+import com.preproject.server.user.entity.User;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -10,6 +13,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @ToString
@@ -43,4 +48,32 @@ public class Question {
             columnDefinition = "datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP")
     @LastModifiedDate
     private LocalDateTime updateAt;
+
+    /* 연관 관계 */
+
+    @Setter
+    @ManyToOne(optional = true, fetch = FetchType.LAZY)
+    private User user;
+
+    @ToString.Exclude
+    @OrderBy("answerId")
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answers = new ArrayList<>();
+
+    @ToString.Exclude
+    @OrderBy("questionCommentId")
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<QuestionComment> questionComments = new ArrayList<>();
+
+    @ToString.Exclude
+    @OrderBy("questionVoteId")
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<QuestionVote> questionVotes = new ArrayList<>();
+
+    @ToString.Exclude
+    @OrderBy("questionTagId")
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<QuestionTag> questionTags = new ArrayList<>();
+
+
 }
--- server/src/main/java/com/preproject/server/question/entity/QuestionTag.java
+++ server/src/main/java/com/preproject/server/question/entity/QuestionTag.java
@@ -1,7 +1,7 @@
-package com.preproject.server.tag.entity;
+package com.preproject.server.question.entity;
 
 
-import com.preproject.server.question.entity.QuestionTag;
+import com.preproject.server.tag.entity.Tag;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,43 +11,40 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @ToString
 @Table(indexes = {
         @Index(columnList = "createAt"),
         @Index(columnList = "updateAt")
-}, name = "TAGS")
+})
 @EntityListeners(AuditingEntityListener.class)
 @Entity
-public class Tag {
+public class QuestionTag {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long tagId;
+    private Long questionTagId;
 
-    @Setter
-    @Column(nullable = false)
-    private String tag;
+
 
     @Column(nullable = false, insertable = false, updatable = false,
             columnDefinition = "datetime default CURRENT_TIMESTAMP")
     @CreatedDate
     private LocalDateTime createAt;
 
-
     @Column(nullable = false, insertable = false, updatable = false,
             columnDefinition = "datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP")
     @LastModifiedDate
     private LocalDateTime updateAt;
 
-
     /* 연관 관계 */
 
-    @ToString.Exclude
-    @OrderBy("questionTagId")
-    @OneToMany(mappedBy = "tag", cascade = CascadeType.REMOVE)
-    private List<QuestionTag> questionTags = new ArrayList<>();
+    @Setter
+    @ManyToOne(optional = true, fetch = FetchType.LAZY)
+    private Question question;
+
+    @Setter
+    @ManyToOne(optional = true, fetch = FetchType.LAZY)
+    private Tag tag;
 }
