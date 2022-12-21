ca5ab57c75e0f9426df2f4c75332d09eafe75efc
Choiyu330 <jenny4063@naver.com>
2022-12-21 15:05:33 +0900

    feat: AnswerCommentController - 답변 Comment

---
+++ server/src/main/java/com/preproject/server/answer/controller/AnswerCommentController.java
@@ -0,0 +1,31 @@
+package com.preproject.server.answer.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/answer-comment")
+public class AnswerCommentController {
+
+    // 1' 답변의 Comment 생성
+    @PostMapping("/{answerId}")
+    public ResponseEntity postComment(@PathVariable("answerId") Long answerId
+
+    ) {
+        return null;
+    }
+
+    // 1' Comment 수정
+    @PatchMapping("/comment/{commentId}")
+    public ResponseEntity patchComment(@PathVariable("commentId") Long commentId
+    ) {
+        return null;
+    }
+
+    // 1' Comment 삭제
+    @DeleteMapping("/comment/{commentId}")
+    public ResponseEntity deleteComment(@PathVariable("commentId") Long commentId
+    ) {
+        return null;
+    }
+}
