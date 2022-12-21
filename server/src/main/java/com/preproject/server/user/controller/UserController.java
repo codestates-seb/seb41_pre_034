20c6ad2f0935af50d0f6ab88c4927dc7b51e1330
sussa3007 <sussa1933@gmail.com>
2022-12-21 13:42:53 +0900

    feat: UserController - 사용자 단건/페이지 조회(GET)

---
+++ server/src/main/java/com/preproject/server/user/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.preproject.server.user.controller;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    /* 사용자 단건 조회 */
+    @GetMapping("/{userId}")
+    public ResponseEntity getUser(
+            @PathVariable("userId") Long userId
+    ) {
+        return null;
+    }
+
+    /* 사용자 페이지 전체 조회 */
+    @GetMapping
+    public ResponseEntity getUsers(
+            @PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.DESC)
+            Pageable pageable
+    ) {
+        return null;
+    }
+
+}
