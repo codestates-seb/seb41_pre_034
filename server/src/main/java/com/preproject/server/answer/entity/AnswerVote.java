21b1a3b942dc363687474eba5767c4907917dadc
Choiyu330 <jenny4063@naver.com>
2022-12-21 17:24:02 +0900

    feat: AnswerVote - Entity 생성

---
+++ server/src/main/java/com/preproject/server/answer/entity/AnswerVote.java
@@ -0,0 +1,37 @@
+package com.preproject.server.answer.entity;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "createAt"),
+        @Index(columnList = "updateAt")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class AnswerVote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long answerVoteId;
+
+    @Column(nullable = false, insertable = false, updatable = false,
+            columnDefinition = "datetime default CURRENT_TIMESTAMP")
+    @CreatedDate
+    private LocalDateTime createAt;
+
+    @Column(nullable = false, insertable = false, updatable = false,
+            columnDefinition = "datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP")
+    @LastModifiedDate
+    private LocalDateTime updateAt;
+
+    //TODO answer_vote_status ENUM
+
+}
