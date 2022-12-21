ccb33195aa64a079befb4bce6dbfb386dc43019f
Choiyu330 <jenny4063@naver.com>
2022-12-21 17:23:47 +0900

    feat: Answer - Entity 생성

---
+++ server/src/main/java/com/preproject/server/answer/entity/Answer.java
@@ -0,0 +1,40 @@
+package com.preproject.server.answer.entity;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "createAt"),
+        @Index(columnList = "updateAt")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long answerId;
+
+    @Column(nullable = false, columnDefinition = "LONGTEXT")
+    private String body;
+
+    @Column(nullable = false)
+    private Boolean check;
+
+    @Column(nullable = false, insertable = false, updatable = false,
+            columnDefinition = "datetime default CURRENT_TIMESTAMP")
+    @CreatedDate
+    private LocalDateTime createAt;
+
+    @Column(nullable = false, insertable = false, updatable = false,
+            columnDefinition = "datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP")
+    @LastModifiedDate
+    private LocalDateTime updateAt;
+}
