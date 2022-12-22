01e029f181bfbe8495dc855b2dea9402ff48cacb
sussa3007 <sussa1933@gmail.com>
2022-12-22 16:20:41 +0900

    feat: User & Tag Controller Stub 반환 구현

--- server/src/main/java/com/preproject/server/user/controller/UserController.java
+++ server/src/main/java/com/preproject/server/user/controller/UserController.java
@@ -1,21 +1,35 @@
 package com.preproject.server.user.controller;
 
+import com.preproject.server.dto.PageResponseDto;
+import com.preproject.server.dto.ResponseDto;
+import com.preproject.server.user.dto.UserPostDto;
+import com.preproject.server.user.dto.UserResponseDto;
+import com.preproject.server.utils.StubDtoUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final StubDtoUtils stubDtoUtils;
 
     /* 사용자 단건 조회 */
     @GetMapping("/{userId}")
     public ResponseEntity getUser(
             @PathVariable("userId") Long userId
     ) {
-        return null;
+        return new ResponseEntity<>(
+                ResponseDto.of(stubDtoUtils.createUserResponseDto()),
+                HttpStatus.OK
+                );
     }
 
     /* 사용자 페이지 전체 조회 */
@@ -24,15 +38,27 @@ public class UserController {
             @PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.DESC)
             Pageable pageable
     ) {
-        return null;
+        Page<UserResponseDto> userResponseDtoPage =
+                stubDtoUtils.createUserResponseDtoPage(pageable);
+        PageResponseDto response = PageResponseDto.of(
+                userResponseDtoPage.getContent()
+                , userResponseDtoPage);
+
+        return new ResponseEntity<>(
+                response,
+                HttpStatus.OK
+        );
     }
 
     /* 사용자 생성 */
     @PostMapping
     public ResponseEntity createUser(
-            // Todo UserPostDto
+            @RequestBody UserPostDto userPostDto
     ) {
-        return null;
+        return new ResponseEntity<>(
+                ResponseDto.of(stubDtoUtils.createUserDto()),
+                HttpStatus.OK
+        );
     }
 
     /* 사용자 정보 수정 */
@@ -40,13 +66,16 @@ public class UserController {
     public ResponseEntity patchUser(
             @PathVariable("userId") Long userId
     ) {
-        return null;
+        return new ResponseEntity<>(
+                ResponseDto.of(stubDtoUtils.createUserResponseDto()),
+                HttpStatus.OK
+        );
     }
 
     /* 사용자 전체 삭제 */
     @DeleteMapping
     public ResponseEntity deleteUsers() {
-        return null;
+        return ResponseEntity.ok().build();
     }
 
     /* 사용자 삭제 */
@@ -54,6 +83,6 @@ public class UserController {
     public ResponseEntity deleteUser(
             @PathVariable("userId") Long userId
     ) {
-        return null;
+        return ResponseEntity.ok().build();
     }
 }
