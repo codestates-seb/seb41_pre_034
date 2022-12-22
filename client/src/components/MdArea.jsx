072afb8e804a2e0be430c4c5f2699538e58a82a9
강진원 <jinwon@gangjin-won-ui-MacBookAir.local>
2022-12-22 21:08:18 +0900

    feat: Markdown Editor 구현

--- client/src/components/MdArea.jsx
+++ client/src/components/MdArea.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import MDEditor from '@uiw/react-md-editor';
 
-function MdArea(props) {
-  return <div></div>;
+function MdArea() {
+  // markdown.current.value();
+  // new StacksEditor(markdown, '*Your* **markdown** here', {});
+
+  const [value, setValue] = React.useState('**Hello world!!!**');
+  return (
+    <div className="container">
+      <MDEditor value={value} onChange={setValue} />
+      {/* <MDEditor.Markdown source={value} style={{ whiteSpace: 'pre-wrap' }} /> */}
+    </div>
+  );
 }
 
 export default MdArea;
