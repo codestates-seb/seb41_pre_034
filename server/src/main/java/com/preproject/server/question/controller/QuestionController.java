d0a0bb714a8b652d1a6a48f96bb86ffe6897f37f
wondo07 <110904783+wondo07@users.noreply.github.com>
2022-12-22 18:09:17 +0900

    feat: QuestionController stub 데이터 추가

--- server/src/main/java/com/preproject/server/question/controller/QuestionController.java
+++ server/src/main/java/com/preproject/server/question/controller/QuestionController.java
@@ -4,6 +4,10 @@ import com.preproject.server.dto.PageResponseDto;
 import com.preproject.server.dto.ResponseDto;
 import com.preproject.server.question.dto.QuestionPostDto;
 import com.preproject.server.question.dto.QuestionResponseDto;
+import com.preproject.server.user.dto.UserResponseDto;
+import com.preproject.server.utils.StubDtoUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -13,7 +17,10 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/questions")
+@RequiredArgsConstructor
 public class QuestionController {
+
+    private final StubDtoUtils stubDtoUtils;
 
     //    질문 생성
     @PostMapping
@@ -21,23 +28,21 @@ public class QuestionController {
             @RequestBody QuestionPostDto questionPostDto
     ) {
         return new ResponseEntity<>(
-                ResponseDto.of(new QuestionResponseDto()),
+                ResponseDto.of(stubDtoUtils.createQuestionDto()),
                 HttpStatus.CREATED);
     }
 
 //    질문 전체 삭제
     @DeleteMapping
     public ResponseEntity deleteQuestion(){
-        return new ResponseEntity<>(
-                ResponseDto.of(new QuestionResponseDto()),
-                HttpStatus.OK);
+        return ResponseEntity.ok().build();
     }
 
 //    질문 단건 조회
     @GetMapping("/{qestionId}")
     public ResponseEntity getQuestion(@PathVariable Long questionId){
         return new ResponseEntity<>(
-                ResponseDto.of(new QuestionResponseDto()),
+                ResponseDto.of(stubDtoUtils.createQuestionResponseDto()),
                 HttpStatus.OK);
     }
 
@@ -47,16 +52,14 @@ public class QuestionController {
         ,@RequestBody QuestionPostDto questionPostDto
     ){
         return new ResponseEntity<>(
-                ResponseDto.of(new QuestionResponseDto()),
+                ResponseDto.of(stubDtoUtils.createQuestionResponseDto()),
                 HttpStatus.OK);
     }
 
 //    질문 삭제
     @DeleteMapping("/{questionId}")
     public ResponseEntity deleteQuestion(@PathVariable Long questionId){
-        return new ResponseEntity<>(
-                ResponseDto.of(new QuestionResponseDto()),
-                HttpStatus.OK);
+        return ResponseEntity.ok().build();
     }
 
 //    질문 전체 조회 페이지
@@ -65,9 +68,16 @@ public class QuestionController {
             @PageableDefault(page = 0, size = 10, sort = "questionId", direction = Sort.Direction.DESC)
             Pageable pageable
     ){
+        Page<QuestionResponseDto> questionResponseDtoPage =
+                stubDtoUtils.createQuestionResponseDtoPage(pageable);
+        PageResponseDto response = PageResponseDto.of(
+                questionResponseDtoPage.getContent()
+                , questionResponseDtoPage);
+
         return new ResponseEntity<>(
-                PageResponseDto.of(null,null),
-                HttpStatus.OK);
+                response,
+                HttpStatus.OK
+        );
     }
 
 }
