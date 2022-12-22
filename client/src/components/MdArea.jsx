b55ae225bee222b3cb6ff7210d36530d4b9d37fc
강진원 <jinwon@gangjin-won-ui-MacBookAir.local>
2022-12-22 21:17:11 +0900

    chore: 주석제거

--- client/src/components/MdArea.jsx
+++ client/src/components/MdArea.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
 function MdArea() {
-  // markdown.current.value();
-  // new StacksEditor(markdown, '*Your* **markdown** here', {});
-
   const [value, setValue] = React.useState('**Hello world!!!**');
   return (
     <div className="container">
       <MDEditor value={value} onChange={setValue} />
-      {/* <MDEditor.Markdown source={value} style={{ whiteSpace: 'pre-wrap' }} /> */}
     </div>
   );
 }
