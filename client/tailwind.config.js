a947317e838ca6e3ad52ae215b656e46c9aaebf9
ansmeer008 <ansmeer008@gmail.com>
2022-12-22 13:02:38 +0900

    feat:Sidebar 구현

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
     extend: {},
+    backgroundColor: {
+      focused: '#f1f1f3',
+    },
+    fontWeight: {
+      focused: '700',
+    },
+    borderColor: {
+      focused: '#f38226',
+    },
+    borderWidth: {
+      focused: '3px',
+    },
   },
   plugins: [],
 };
