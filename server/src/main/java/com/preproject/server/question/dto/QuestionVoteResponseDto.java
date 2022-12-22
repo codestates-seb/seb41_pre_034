ab25a4260d0ca209284192669e95476eb40202b6
sussa3007 <sussa1933@gmail.com>
2022-12-22 10:47:29 +0900

    feat: Question 소속 Entity Response Dto 구현

---
+++ server/src/main/java/com/preproject/server/question/dto/QuestionVoteResponseDto.java
@@ -0,0 +1,14 @@
+package com.preproject.server.question.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+public class QuestionVoteResponseDto {
+
+    private Long userId;
+
+}
