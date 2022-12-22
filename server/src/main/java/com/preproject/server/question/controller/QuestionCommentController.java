ec1125b785432f166097fb761af30c14cfac4755
wondo07 <110904783+wondo07@users.noreply.github.com>
2022-12-22 16:20:43 +0900

    feat: QuestionCommentController basic 구현

--- server/src/main/java/com/preproject/server/question/controller/QuestionCommentController.java
+++ server/src/main/java/com/preproject/server/question/controller/QuestionCommentController.java
@@ -1,6 +1,12 @@
 package com.preproject.server.question.controller;
 
 
+import com.preproject.server.dto.ResponseDto;
+import com.preproject.server.question.dto.QuestionCommentPatchDto;
+import com.preproject.server.question.dto.QuestionCommentPostDto;
+import com.preproject.server.question.dto.QuestionCommentResponseDto;
+import com.preproject.server.question.dto.QuestionResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,22 +16,27 @@ public class QuestionCommentController {
 
     // 질문의 comment 생성
     @PostMapping("/{questionId}")
-    public ResponseEntity postQuestionComment(@PathVariable Long questionId){
-             // Todo QuestionCommentPostDto
-        return null;
+    public ResponseEntity postQuestionComment(@PathVariable Long questionId
+             ,@RequestBody QuestionCommentPostDto questionCommentPostDto){
+        return new ResponseEntity<>(
+                ResponseDto.of(new QuestionCommentResponseDto()),
+                HttpStatus.CREATED);
     }
 
 //  질문의  Comment 수정
     @PatchMapping("/comment/{questionCommentId}")
     public ResponseEntity patchQuestionComment(@PathVariable Long questionCommentId
-               // Todo QuestionCommentPatchDto
-    ){
-        return null;
+               , @RequestBody QuestionCommentPatchDto questionCommentPatchDto){
+        return new ResponseEntity<>(
+                ResponseDto.of(new QuestionCommentResponseDto()),
+                HttpStatus.OK);
     }
 
 //  질문의 Comment 삭제
     @DeleteMapping("/comment/{questionCommentId}")
     public ResponseEntity deleteQuestionComment(@PathVariable Long questionCommentId){
-        return null;
+        return new ResponseEntity<>(
+                ResponseDto.of(new QuestionCommentResponseDto()),
+                HttpStatus.OK);
     }
 }
