fa5cb0e58968b20cd5486727e2aa7594cf502a78
wondo07 <110904783+wondo07@users.noreply.github.com>
2022-12-22 16:21:22 +0900

    feat: QuestionVoteController basic 구현

--- server/src/main/java/com/preproject/server/question/controller/QuestionVoteController.java
+++ server/src/main/java/com/preproject/server/question/controller/QuestionVoteController.java
@@ -1,5 +1,11 @@
 package com.preproject.server.question.controller;
 
+import com.preproject.server.dto.ResponseDto;
+import com.preproject.server.question.dto.QuestionResponseDto;
+import com.preproject.server.question.dto.QuestionVotePatchDto;
+import com.preproject.server.question.dto.QuestionVotePostDto;
+import com.preproject.server.question.dto.QuestionVoteResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,16 +15,20 @@ public class QuestionVoteController {
 
 //    질문의 추천 생성(UP/NONE/DOWN)
     @PostMapping("/{questionId}")
-    public ResponseEntity postQuestionvote(@PathVariable Long questionId){
-            // Todo QuestionVotePostDto
-        return null;
+    public ResponseEntity postQuestionvote(@PathVariable Long questionId
+                        , @RequestBody QuestionVotePostDto questionVotePostDto){
+        return new ResponseEntity<>(
+                ResponseDto.of(new QuestionVoteResponseDto()),
+                HttpStatus.CREATED);
+
     }
 
 //    추천 (UP/NONE/DOWN) 수정
     @PatchMapping("/vote/{questionVoteId}")
     public ResponseEntity patchQuestionvote(@PathVariable Long questionVoteId
-            // Todo QuestionVotePatchDto
-    ){
-        return null;
+            , @RequestBody QuestionVotePatchDto questionVotePatchDto){
+        return new ResponseEntity<>(
+                ResponseDto.of(new QuestionVoteResponseDto()),
+                HttpStatus.CREATED);
     }
 }
