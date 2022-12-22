88fa767bc0c05fffcccbd3c45ebdcc9f1c3d3c87
Choiyu330 <jenny4063@naver.com>
2022-12-22 18:11:28 +0900

    feat: AnswerController stub 추가

--- server/src/main/java/com/preproject/server/answer/controller/AnswerController.java
+++ server/src/main/java/com/preproject/server/answer/controller/AnswerController.java
@@ -4,6 +4,7 @@ import com.preproject.server.answer.dto.AnswerPatchDto;
 import com.preproject.server.answer.dto.AnswerPostDto;
 import com.preproject.server.answer.dto.AnswerResponseDto;
 import com.preproject.server.dto.ResponseDto;
+import com.preproject.server.utils.StubDtoUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,23 +13,32 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/answers")
 public class AnswerController {
 
+    private final StubDtoUtils stubDtoUtils;
+
+    public AnswerController(StubDtoUtils stubDtoUtils) {
+        this.stubDtoUtils = stubDtoUtils;
+    }
+
+    /* 답변 생성 */
     @PostMapping
     public ResponseEntity postAnswer(@RequestBody AnswerPostDto answerPostDto
     ) {
         return new ResponseEntity<>(
-                ResponseDto.of(new AnswerResponseDto()),
+                ResponseDto.of(stubDtoUtils.createAnswerDto()),
                 HttpStatus.CREATED);
     }
 
+    /* 답변 수정 */
     @PatchMapping("/{answerId}")
     public ResponseEntity patchAnswer(@PathVariable("answerId") Long answerId,
                                       @RequestBody AnswerPatchDto answerPatchDto
     ) {
         return new ResponseEntity<>(
-                ResponseDto.of(new AnswerResponseDto()),
+                ResponseDto.of(stubDtoUtils.createAnswerResponseDto()),
                 HttpStatus.OK);
     }
 
+    /* 답변 삭제 */
     @DeleteMapping("/{answerId}")
     public ResponseEntity deleteAnswer(@PathVariable("answerId") Long answerId
     ) {
