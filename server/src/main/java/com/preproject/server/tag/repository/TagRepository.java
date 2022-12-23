47f9180a02ae16b12065d967033b4fcd4a5d591e
sussa3007 <sussa1933@gmail.com>
2022-12-23 15:12:58 +0900

    feat: User&Tag Repository 검색 조건 추가

--- server/src/main/java/com/preproject/server/tag/repository/TagRepository.java
+++ server/src/main/java/com/preproject/server/tag/repository/TagRepository.java
@@ -1,7 +1,16 @@
 package com.preproject.server.tag.repository;
 
 import com.preproject.server.tag.entity.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    Optional<Tag> findByTag(String tag);
+
+    Page<Tag> findAll(Pageable pageable);
+
 }
--- server/src/main/java/com/preproject/server/user/repository/UserRepository.java
+++ server/src/main/java/com/preproject/server/user/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.preproject.server.user.repository;
 
 import com.preproject.server.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,5 +10,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
+    Page<User> findAll(Pageable pageable);
 
 }
