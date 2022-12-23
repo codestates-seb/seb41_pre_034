c031982e7568b1d9f1fd62d4f3d351427f0164e1
jsh3418 <albert5428@gmail.com>
2022-12-23 13:59:18 +0900

    fix: Sidebar 스티키 옵션 수정

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,17 +2,20 @@ import React, { useState } from 'react';
 
 function Sidebar() {
   const [currentMenu, setCurrentMenu] = useState(0);
-  //클릭 이벤트가 발생하면 해당 li 요소로 focus가 이동한다
+  // 클릭 이벤트가 발생하면 해당 li 요소로 focus가 이동한다
   const menuArr = [{ name: 'Home' }, { name: 'Tags' }, { name: 'Users' }];
+
   const selectMenuHandler = (index) => {
     setCurrentMenu(index);
   };
+
   const basicLiClassName =
-    'h-[30px] py-[4px] pl-[8px] pr-[4px] text-[#525960] text-[13px] cursor-pointer';
+    'h-[30px] py-[4px] pl-[8px] pr-[4px] text-[#525960] text-[13px] cursor-pointer w-[164px]';
+
   return (
-    <div className="pt-[24px] mb-[8px] w-[164px] felx-col h-[100vh] relative bg-[#feffff] border-r-[1px] br-[#d7d9dc]">
-      <nav className="sticky top-[50px]">
-        <ol className="flex-col justify-evenly h-[200px]">
+    <div className="sticky w-auto top-[50px] mb-[8px] max-h-[calc(100vh-50px-322px)] pt-[24px] bg-[#feffff]">
+      <nav>
+        <ol>
           {menuArr.map((el, idx) => {
             return (
               <li
