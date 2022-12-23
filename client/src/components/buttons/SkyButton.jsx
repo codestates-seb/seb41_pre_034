affc161d6c93b4a3678c7494135ce2adac72f46b
jsh3418 <albert5428@gmail.com>
2022-12-23 21:29:33 +0900

    design: SkyButton 부모 요소에 맞게 크기 조절되도록 수정

--- client/src/components/buttons/SkyButton.jsx
+++ client/src/components/buttons/SkyButton.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function SkyButton(props) {
   return (
-    <div className="block">
+    <div>
       <a
         href="/"
-        className="bg-[#e1ecf4] border-[1px] border-solid	rounded border-[#39739d] text-[#39739d] p-[10px] text-[13px] font-normal leading-4 relative hover:bg-[#b3d3ea]"
+        className="inline-block text-center w-full h-full bg-[#e1ecf4] border-[1px] border-solid	rounded border-[#39739d] text-[#39739d] p-[10px] text-[13px] font-normal leading-4 relative hover:bg-[#b3d3ea]"
       >
         {props.text}
       </a>
