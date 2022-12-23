d8bcd37bf4cf3e43776830d09867b5da002b6ee1
wondo07 <110904783+wondo07@users.noreply.github.com>
2022-12-23 22:50:20 +0900

    refactoring: QuestionCommentService 구현

--- server/src/main/java/com/preproject/server/question/service/QuestionCommentService.java
+++ server/src/main/java/com/preproject/server/question/service/QuestionCommentService.java
@@ -1,24 +1,38 @@
 package com.preproject.server.question.service;
 
+import com.preproject.server.constant.ErrorCode;
+import com.preproject.server.exception.ServiceLogicException;
+import com.preproject.server.question.dto.QuestionResponseDto;
 import com.preproject.server.question.entity.Question;
 import com.preproject.server.question.entity.QuestionComment;
 import com.preproject.server.question.repository.QuestionCommentRepository;
+import com.preproject.server.question.repository.QuestionRepository;
+import com.preproject.server.user.entity.User;
+import com.preproject.server.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class QuestionCommentService {
 
-    @Autowired
-    private QuestionCommentRepository questionCommentRepository;
 
-    public QuestionComment save(QuestionComment questionComment, Long questionId) {
-        if(questionComment.getQuestion().getQuestionId()==questionId){
-            questionCommentRepository.save(questionComment);
-        }
-        return questionComment;
+    private final QuestionCommentRepository questionCommentRepository;
+
+    private final UserRepository userRepository;
+
+    private final QuestionRepository questionRepository;
+
+    public QuestionComment save(QuestionComment questionComment) {
+
+        QuestionComment save = questionCommentRepository.save(questionComment);
+
+        return save;
     }
 
     public void delete(Long questionCommentId) {
@@ -31,8 +45,15 @@ public class QuestionCommentService {
         Optional<QuestionComment> optionalQuestionComment =
                 questionCommentRepository.findById(questionId);
         QuestionComment findQuestionComment =
-                optionalQuestionComment.orElseThrow(NullPointerException::new);
+                optionalQuestionComment.orElseThrow(() -> new ServiceLogicException(ErrorCode.QUESTION_NOT_FOUND));
 
         return findQuestionComment;
+    }
+
+    private User verifiedUserById(Long userId) {
+        Optional<User> findUser = userRepository.findById(userId);
+        return findUser.orElseThrow(
+                () -> new ServiceLogicException(ErrorCode.QUESTION_NOT_FOUND)
+        );
     }
 }
