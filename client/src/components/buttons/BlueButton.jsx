b07d01d6b267b7514c2a8c5f524cc53c77ba5e12
jsh3418 <albert5428@gmail.com>
2022-12-23 22:16:06 +0900

    fix: 중첩된 a태그 삭제
    error message : Warning: validateDOMNesting(...): <a> cannot appear as a descendant of <a>.

--- client/src/components/buttons/BlueButton.jsx
+++ client/src/components/buttons/BlueButton.jsx
@@ -2,13 +2,8 @@ import React from 'react';
 
 function BlueButton(props) {
   return (
-    <div>
-      <a
-        href="/"
-        className="inline-block text-center w-full h-full bg-[#0a95ff] border-[1px] border-solid border-[#0a95ff] p-[10px] rounded border-transparent text-[#ffffff] text-[13px] font-normal leading-4 relative hover:bg-[#39739d]"
-      >
-        {props.text}
-      </a>
+    <div className="inline-block text-center w-full h-full bg-[#0a95ff] border-[1px] border-solid border-[#0a95ff] p-[10px] rounded border-transparent text-[#ffffff] text-[13px] font-normal leading-4 relative hover:bg-[#39739d]">
+      {props.text}
     </div>
   );
 }
--- client/src/components/buttons/SkyButton.jsx
+++ client/src/components/buttons/SkyButton.jsx
@@ -2,13 +2,8 @@ import React from 'react';
 
 function SkyButton(props) {
   return (
-    <div>
-      <a
-        href="/"
-        className="inline-block text-center w-full h-full bg-[#e1ecf4] border-[1px] border-solid	rounded border-[#39739d] text-[#39739d] p-[10px] text-[13px] font-normal leading-4 relative hover:bg-[#b3d3ea]"
-      >
-        {props.text}
-      </a>
+    <div className="inline-block text-center w-full h-full bg-[#e1ecf4] border-[1px] border-solid	rounded border-[#39739d] text-[#39739d] p-[10px] text-[13px] font-normal leading-4 relative hover:bg-[#b3d3ea]">
+      {props.text}
     </div>
   );
 }
