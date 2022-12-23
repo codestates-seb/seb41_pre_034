1c8370eefd8a9cc8e8fb2d6ad1c79a6ef79ddd13
jsh3418 <albert5428@gmail.com>
2022-12-23 10:34:46 +0900

    design: Navbar maxWidth, z-index 수정

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -65,8 +65,8 @@ function Navbar(props) {
 
   return (
     <>
-      <header className="box-border h-[50px] fixed left-0 top-0 min-w-[auto] shadow-md w-full z-50 bg-white	flex border-t-[3px] border-[#f48225] items-center">
-        <div className="w-[97.2307692rem] max-w-full h-full flex my-0 mx-auto items-center box-border">
+      <header className="box-border h-[50px] fixed left-0 top-0 min-w-[auto] shadow-md w-full z-[5050] bg-white	flex border-t-[3px] border-[#f48225] items-center">
+        <div className="w-[97.2307692rem] max-w-[1264px] h-full flex my-0 mx-auto items-center box-border">
           <a
             href="#"
             className="py-0 px-2 h-full flex items-center bg-transparent box-border hover:bg-[#e3e6e8]"
