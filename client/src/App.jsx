791912dad9106caf15da4215bd5542717daafd92
jsh3418 <albert5428@gmail.com>
2022-12-24 12:01:02 +0900

    chore: route path 상수화

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,21 +9,22 @@ import Tags from './pages/Tags';
 import Users from './pages/Users';
 import QuestionDetail from './pages/QuestionDetail';
 import Navbar from './components/Navbar';
+import ROUTE_PATH from './constants/routePath';
 
 function App() {
   return (
     <Router basename="/">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/addquestion" element={<CreateQuestion />} />
-        <Route path="/editquestion" element={<EditQuestion />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/tags" element={<Tags />} />
-        <Route path="/Users" element={<Users />} />
-        <Route path="/detail" element={<QuestionDetail />} />
+        <Route path={ROUTE_PATH.HOME} element={<Home />} />
+        <Route path={ROUTE_PATH.LOGIN} element={<Login />} />
+        <Route path={ROUTE_PATH.SIGNUP} element={<SignUp />} />
+        <Route path={ROUTE_PATH.ADD_QUESTION} element={<CreateQuestion />} />
+        <Route path={ROUTE_PATH.EDIT_QUESTION} element={<EditQuestion />} />
+        <Route path={ROUTE_PATH.MY_PAGE} element={<MyPage />} />
+        <Route path={ROUTE_PATH.TAGS} element={<Tags />} />
+        <Route path={ROUTE_PATH.USERS} element={<Users />} />
+        <Route path={ROUTE_PATH.DETAIL} element={<QuestionDetail />} />
       </Routes>
     </Router>
   );
---
+++ client/src/constants/routePath.jsx
@@ -0,0 +1,13 @@
+const ROUTE_PATH = {
+  HOME: '/',
+  LOGIN: '/login',
+  SIGNUP: '/signup',
+  ADD_QUESTION: '/addquestion',
+  EDIT_QUESTION: '/editquestion',
+  MY_PAGE: '/mypage',
+  TAGS: '/tags',
+  USERS: '/users',
+  DETAIL: '/detail',
+};
+
+export default ROUTE_PATH;
