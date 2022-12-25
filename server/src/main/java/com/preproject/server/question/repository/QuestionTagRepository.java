da167ee83a55f0f3cfa6781de8b0bfabee5a3414
sussa3007 <sussa1933@gmail.com>
2022-12-25 15:24:55 +0900

    feat: Tag를 소유한 Question 검색 기능 추가

--- server/src/main/java/com/preproject/server/question/repository/QuestionTagRepository.java
+++ server/src/main/java/com/preproject/server/question/repository/QuestionTagRepository.java
@@ -1,7 +1,12 @@
 package com.preproject.server.question.repository;
 
 import com.preproject.server.question.entity.QuestionTag;
+import com.preproject.server.tag.entity.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionTagRepository extends JpaRepository<QuestionTag, Long> {
+
+    Page<QuestionTag> findAllByTag(Tag tag, Pageable pageable);
 }
