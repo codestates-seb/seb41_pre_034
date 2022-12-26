3df83fd3ef745e74520f6319ae3abe30b4b6f6eb
진원 <89727516+z1nun@users.noreply.github.com>
2022-12-26 11:22:51 +0900

    Chore: 주석삭제

--- client/src/components/Tabs.jsx
+++ client/src/components/Tabs.jsx
@@ -4,7 +4,6 @@ import { useState } from 'react';
 function Tabs(props) {
   const [currentTab, setcurrentTab] = useState(0);
 
-  //users나 Home에서 배열의 length가 다를텐데 props로 배열들을 어떻게 받아줘야하는지.
   const menuArr = props.menuArr;
 
   const selectMenuHandler = (index) => {
@@ -22,8 +21,6 @@ function Tabs(props) {
     'box-content flex p-[10.4px] mr-[-1px] mb-[-1px] hover:bg-[#f8f9f9] h-[15.794px] border-[#838C95] border-[1px] rounded-tr rounded-br';
   const lastElfocused =
     'box-content flex p-[10.4px]  mr-[-1px] mb-[-1px] text-slate-700 bg-[#E3E6E8] h-[15.794px] border-[#838C95] border-[1px] rounded-tr rounded-br';
-  // border-radius관련, 부모가 div 태그이고, 자식으로 a태그들이 있는데 a태그에 각각 border-radius가 적용되있는 것이아니라 div태그에 border-radius가 있음
-  // border-[#838C95] border-[1px]
   const last = menuArr.length - 1;
 
   return (
@@ -61,42 +58,3 @@ function Tabs(props) {
 }
 
 export default Tabs;
-
-/*
-import React from 'react';
-import { useState } from 'react';
-
-function Tabs(props) {
-  const [currentTab, setcurrentTab] = useState(0);
-
-  //users나 Home에서 배열의 length가 다를텐데 props로 배열들을 어떻게 받아줘야하는지.
-  const menuArr = [{ name: 'Popular' }, { name: 'Name' }, { name: 'New' }];
-
-  const selectMenuHandler = (index) => {
-    setcurrentTab(index);
-  };
-  const submenu =
-    'box-content flex p-[10.4px] mr-[-1px] mb-[-1px] hover:bg-[#f8f9f9] h-[15.794px] border-[#838C95] border-[1px]';
-  const focused =
-    'box-content flex p-[10.4px]  mr-[-1px] mb-[-1px] text-slate-700 bg-[#E3E6E8] h-[15.794px] border-[#838C95] border-[1px] ';
-  // border-radius관련, 부모가 div 태그이고, 자식으로 a태그들이 있는데 a태그에 각각 border-radius가 적용되있는 것이아니라 div태그에 border-radius가 있음
-  // border-[#838C95] border-[1px]
-  return (
-    <div className="box-content flex flex-wrap mb-[1px] items-center justify-center text-[13px] border-[#838C95] border-[1px] h-[37.594px] rounded">
-      {menuArr.map((el, index) => {
-        return (
-          <a
-            key={index}
-            className={currentTab === index ? focused : submenu}
-            onClick={() => selectMenuHandler(index)}
-          >
-            {el.name}
-          </a>
-        );
-      })}
-    </div>
-  );
-}
-
-export default Tabs;
-*/
