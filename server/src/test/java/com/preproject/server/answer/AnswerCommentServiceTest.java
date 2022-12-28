af8cccb91450b3eb5696a4ec08b6aa0cf10d3536
Choiyu330 <jenny4063@naver.com>
2022-12-29 00:43:59 +0900

    feat: AnswerCommentServiceTest 구현

---
+++ server/src/test/java/com/preproject/server/answer/AnswerCommentServiceTest.java
@@ -0,0 +1,69 @@
+package com.preproject.server.answer;
+
+import com.preproject.server.answer.entity.AnswerComment;
+import com.preproject.server.answer.repository.AnswerCommentRepository;
+import com.preproject.server.answer.service.AnswerCommentService;
+import com.preproject.server.constant.ErrorCode;
+import com.preproject.server.exception.ServiceLogicException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class AnswerCommentServiceTest {
+    @Mock
+    private AnswerCommentRepository answerCommentRepository;
+
+    @InjectMocks
+    private AnswerCommentService answerCommentService;
+
+    @Test
+    @DisplayName("답변 코멘트 수정 테스트")
+    void updateTest() {
+        // given
+        given(answerCommentRepository.findById(anyLong())).willReturn(Optional.empty());
+
+        // when
+        Throwable throwable = Assertions.catchThrowable(
+                () -> answerCommentService.updateComment(
+                        createTestAnswerComment(1L), 1L));
+
+        // then
+        Assertions.assertThat(throwable)
+                .isInstanceOf(ServiceLogicException.class)
+                .hasMessageContaining(ErrorCode.ANSWER_NOT_FOUND.getMessage());
+    }
+
+    @Test
+    @DisplayName("답변 코멘트 삭제 테스트")
+    void deleteTest() {
+        // given
+        given(answerCommentRepository.findById(anyLong())).willReturn(Optional.empty());
+
+        // when
+        Throwable throwable = Assertions.catchThrowable(
+                () -> answerCommentService.delete(1L));
+
+        // Then
+        Assertions.assertThat(throwable)
+                .isInstanceOf(ServiceLogicException.class)
+                .hasMessageContaining(ErrorCode.ANSWER_NOT_FOUND.getMessage());
+    }
+
+    // 테스트 답변 코멘트 생성
+    private AnswerComment createTestAnswerComment(Long answerCommentId) {
+        AnswerComment testComment = new AnswerComment();
+        testComment.setComment("Create Comment");
+
+        return testComment;
+    }
+}
