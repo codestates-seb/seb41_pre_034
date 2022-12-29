0b57127acbcdb37e84e9a8c939fb366839572bb9
sussa3007 <sussa1933@gmail.com>
2022-12-29 18:06:51 +0900

    test: Question & Answer Vote Logic 변경으로 수정

--- server/src/test/java/com/preproject/server/question/service/QuestionVoteServiceTest.java
+++ server/src/test/java/com/preproject/server/question/service/QuestionVoteServiceTest.java
@@ -1,34 +1,19 @@
 package com.preproject.server.question.service;
 
-import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
 import com.preproject.server.constant.ErrorCode;
 import com.preproject.server.constant.VoteStatus;
 import com.preproject.server.exception.ServiceLogicException;
-import com.preproject.server.question.dto.QuestionVotePatchDto;
-import com.preproject.server.question.dto.QuestionVotePostDto;
 import com.preproject.server.question.entity.QuestionVote;
-import com.preproject.server.question.mapper.QuestionMapper;
-import com.preproject.server.question.repository.QuestionRepository;
 import com.preproject.server.question.repository.QuestionVoteRepository;
-import com.preproject.server.user.repository.UserRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-import static com.preproject.server.question.entity.QQuestionVote.questionVote;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
@@ -54,7 +39,7 @@ class QuestionVoteServiceTest {
 
         //when
         Throwable throwable = Assertions.catchThrowable(
-                () -> questionVoteService.patch(createQuestionVote(1L),1L));
+                () -> questionVoteService.updateVote(createQuestionVote(1L),1L));
         //then
         Assertions.assertThat(throwable)
                 .isInstanceOf(ServiceLogicException.class)
