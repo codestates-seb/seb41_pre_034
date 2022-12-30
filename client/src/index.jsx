539dfec7f2f803af4945dcd490c0fba04be7e2e6
jsh3418 <albert5428@gmail.com>
2022-12-30 16:30:28 +0900

    fix: get 요청 두번씩 하던 오류 수정

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -14,13 +14,11 @@ const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
